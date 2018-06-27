9bec4e288fc65b2733d7df988847fe2ec421a36f
Eeshwar Mann <emann@myseneca.ca>
2018-06-27 06:47:21 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,17 @@
+
+
+/*********************************************************************************
+* WEB422 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Eeshwar Mann Student ID: 103819164 Date: April 11. 2018
+*
+* Heroku Link: _______________________________________________________________
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,     
+    ContentComponent,
+    FooterComponent,
+    HomeComponent,
+    
+
+    PageNotFoundComponent,
+
+   
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
